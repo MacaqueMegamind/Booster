aee04a2de42e2266e52c4ee11547309656a72cd7
vanekForest <zazc256@gmail.com>
2024-04-08 14:27:29 +0300

    feat: add product to forward/cart

--- backend/test2.py
+++ backend/test2.py
@@ -5,8 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
 
-
-
 import time
 
 driver = Driver()
@@ -19,7 +17,7 @@ SIGN_IN_BUTTON = "//button[@type='submit']"
 
 driver.get('https://www.ozon.ru/ozonid')
 WebDriverWait(driver, 20).until(ec.visibility_of_element_located((By.XPATH, AUTH_FORM)))
-driver.find_element(By.XPATH, INPUT_FORM).send_keys("9204669750")
+driver.find_element(By.XPATH, INPUT_FORM).send_keys("9009295422")
 auth = driver.find_element(By.XPATH, AUTH_FORM)
 auth.find_element(By.XPATH, SIGN_IN_BUTTON).click()
 
