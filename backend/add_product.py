e15e7e3a93760dbb6096e14d847d3a2130fe014c
vanekForest <zazc256@gmail.com>
2024-04-08 16:03:21 +0300

    fix: removed async

--- backend/add_product.py
+++ backend/add_product.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-async def add_product(product_link: str, xpath_to_add_cart_button: str):
+def add_product(product_link: str, xpath_to_add_cart_button: str):
     """
     Presses the selected button on the product card using xpath
     :param product_link: Link to the product
@@ -33,5 +33,5 @@ async def add_product(product_link: str, xpath_to_add_cart_button: str):
 product_link = 'https://www.ozon.ru/product/futbolka-1381220957/?advert=ZcmILLXX4DcbpVJdaBPHSM8caLyXEt-oyYVbrvR0dDfkKK6kB5h7iklHuLNo8afqaUaU444KDoA83xBHV4u4vIBDboiHqHDOzTKMZFOGjER_Tk1zHEc3pTJf3s0nV2PuoqiqUNs9SY6PPuaqNVT73wXLgI41jO3MStvtBamcVeodud67lqkhuef6mSgaQA9uS-Ng2t9Q3Sk_79iiObsy8I4hHmVcOe-2Mk6q9ih8RrPH8Ks27Ex9MS7TD3LbU0xHDFZ4WgCKpocWIOf0dBPUo-o_4ieZvqd4zWCNuj2wQOlzAOpstEs7i-bK6Tv4OCKfTh8yQc5pPWM-Nbh7xIgCCm2gIkQlww&avtc=1&avte=2&avts=1712511396&clckid=b93b03d2&keywords=%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D0%BA%D0%B0+%D1%81%D0%BB%D0%B0%D0%B2%D0%B0+%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D0%B8'
 xpath_button_cart = "//button[@class='y9j b200-a0 b200-b2 b200-a4']"
 xpath_button_favorite = "//button[@class='z4j ag00-a0 ag00-a3']"
-asyncio.run(add_product(product_link=product_link, xpath_to_add_cart_button=xpath_button_cart))
-asyncio.run(add_product(product_link=product_link, xpath_to_add_cart_button=xpath_button_favorite))
+add_product(product_link=product_link, xpath_to_add_cart_button=xpath_button_cart)
+add_product(product_link=product_link, xpath_to_add_cart_button=xpath_button_favorite)
