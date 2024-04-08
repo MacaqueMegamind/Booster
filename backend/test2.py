f4ef1db7490dfe0e53d0097732d124aae2040f50
Max Omenes <ukhin.maxim@gmail.com>
2024-04-08 18:13:29 +0300

    add: login

--- backend/test2.py
+++ backend/test2.py
@@ -14,12 +14,18 @@ INPUT_FORM = "//input[@name='autocomplete']"
 SIGN_IN_BUTTON_EN = "//button[@type='submit']/descendant::div[contains(text(), 'Sign in')]"
 SIGN_IN_BUTTON_RU = "//button[@type='submit']/descendant::div[contains(text(), 'Войти')]"
 SIGN_IN_BUTTON = "//button[@type='submit']"
+CODE_INPUT = "//input[@name='otp']"
 
 driver.get('https://www.ozon.ru/ozonid')
 WebDriverWait(driver, 20).until(ec.visibility_of_element_located((By.XPATH, AUTH_FORM)))
-driver.find_element(By.XPATH, INPUT_FORM).send_keys("9009295422")
+driver.find_element(By.XPATH, INPUT_FORM).send_keys("9204669750")
 auth = driver.find_element(By.XPATH, AUTH_FORM)
 auth.find_element(By.XPATH, SIGN_IN_BUTTON).click()
+code = str(input())
+WebDriverWait(driver, 20).until(ec.visibility_of_element_located((By.XPATH, CODE_INPUT)))
+driver.find_element(By.XPATH, CODE_INPUT).send_keys(code)
+
+
 
 
 if input():
