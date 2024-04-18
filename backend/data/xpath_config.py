b6a0a35b6b34eca182e06784470cbad6dc2d785e
Max Omenes <ukhin.maxim@gmail.com>
2024-04-18 18:21:03 +0300

    fix: xpath decorators
    make to improve stability

--- backend/data/xpath_config.py
+++ backend/data/xpath_config.py
@@ -6,7 +6,7 @@ class XPATHLoginEmail:
     StartPage = "https://www.ozon.ru/ozonid"
     AUTH_FORM = "//section[@class='csma-ozon-id-page-anonymous']"
     INPUT_FORM = "//input[@name='email']"
-    SIGN_IN_EMAIL_BUTTON = "//button[@class='cae3 ga00-a']"
+    SIGN_IN_EMAIL_BUTTON = "//div[contains(text(), 'Sign in by email')]/ancestor::button[1]"
     SIGN_IN_BUTTON = "//button[@type='submit']"
     INPUT_CODE_FORM = "//input[@name='otp']"
 
@@ -26,5 +26,5 @@ class XPATHAddProduct:
     """
     These are the favorites and add to cart buttons
     """
-    XPATH_BUTTON_CART = "//button[@class='zj1 b200-a0 b200-b2 b200-a4']"
-    XPATH_BUTTON_FAVORITE = "//button[@class='jz7 ag00-a0 ag00-a3']"
+    XPATH_BUTTON_CART = "//div[contains(text(), 'Добавить в корзину')]/ancestor::button[1]"
+    XPATH_BUTTON_FAVORITE = "//button[@aria-label='Добавить в избранное']"
