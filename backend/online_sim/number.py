f6e03e3ee6da3f504cfd5ffb5fc5b177c6dbf998
forest_pc <zazc256@gmail.com>
2024-05-14 22:15:46 +0300

    fix: fixed send wait code

--- backend/online_sim/number.py
+++ backend/online_sim/number.py
@@ -6,18 +6,20 @@ from backend.data.config import KEY_SIM_API
 class Number:
     def __init__(self):
         self.numbers = NumbersService(apikey=KEY_SIM_API)
-        self.num = self.numbers.get('ozon', number=True)
+        self.tzid = self.numbers.get('ozon', number=True)
+        self.tz = int(self.tzid.get('tzid'))
 
     def get_number(self) -> str:
         """
         Get number phone
         :return: number phone
         """
-        return self.num.get('number')[2:]
+        return self.tzid.get('number')[2:]
 
     def get_code(self) -> str:
         """
         Get wait code
         :return: code
         """
-        return self.numbers.wait_code(self.num)
+        code = self.numbers.wait_code(self.tz)
+        return code
